4b414b172da6db93189bc85953350406645841aa
lais <laisziegler@gmail.com>
2023-08-08 18:26:25 -0300

    initial commit

---
+++ abc_telecom_service/src/main/java/com/laiszig/abc_telecom_service/AbcTelecomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.laiszig.abc_telecom_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcTelecomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcTelecomServiceApplication.class, args);
+	}
+
+}
---
+++ abc_telecom_service/src/test/java/com/laiszig/abc_telecom_service/AbcTelecomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laiszig.abc_telecom_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcTelecomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
