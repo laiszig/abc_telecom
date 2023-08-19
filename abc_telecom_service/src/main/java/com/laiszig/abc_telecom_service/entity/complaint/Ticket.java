eda1796c3a8e000449998a11f50a95a2f09a3ad0
lais <laisziegler@gmail.com>
2023-08-19 15:14:42 -0300

    adjust db structure

--- abc_telecom_service/src/main/java/com/laiszig/abc_telecom_service/entity/complaint/Ticket.java
+++ abc_telecom_service/src/main/java/com/laiszig/abc_telecom_service/entity/complaint/Ticket.java
@@ -1,5 +1,7 @@
 package com.laiszig.abc_telecom_service.entity.complaint;
 
+import com.laiszig.abc_telecom_service.entity.Feedback;
+import com.laiszig.abc_telecom_service.entity.PinCode;
 import com.laiszig.abc_telecom_service.entity.roles.Customer;
 import com.laiszig.abc_telecom_service.entity.roles.Engineer;
 import com.laiszig.abc_telecom_service.entity.roles.Manager;
@@ -7,6 +9,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -16,6 +20,8 @@ import lombok.NoArgsConstructor;
 public class Ticket {
 
     @Id
+    @Column(name = "TICKET_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
@@ -25,8 +31,12 @@ public class Ticket {
     @Column(name = "PROBLEM_TYPE")
     private String problemType;
 
-    @Column(name = "FEEDBACK")
-    private String feedback;
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @OneToOne
+    @JoinColumn(name = "FEEDBACK")
+    private Feedback feedback;
 
     @ManyToOne
     @JoinColumn(name = "MANAGER_ID")
@@ -36,8 +46,17 @@ public class Ticket {
     @JoinColumn(name = "ENGINEER_ID")
     private Engineer engineerAssigned;
 
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "PINCODE_ID")
+    private PinCode pinCode;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "STATUS")
     private Status status;
 
+    @Column(name = "CREATION_DATE")
+    private LocalDateTime creationDate;
+
+    @Column(name = "LAST_UPDATE_DATE")
+    private LocalDateTime lastUpdateDate;
 }
