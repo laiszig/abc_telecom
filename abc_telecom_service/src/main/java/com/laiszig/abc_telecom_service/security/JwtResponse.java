d4fd4a8008e5b1284c0dc939c069ac4cf1e07cfe
lais <laisziegler@gmail.com>
2023-10-03 19:08:08 -0300

    add security config

---
+++ abc_telecom_service/src/main/java/com/laiszig/abc_telecom_service/security/JwtResponse.java
@@ -0,0 +1,15 @@
+package com.laiszig.abc_telecom_service.security;
+
+import java.io.Serializable;
+
+public class JwtResponse implements Serializable {
+    private final String token;
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return this.token;
+    }
+}
